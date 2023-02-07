120d02df8f1689bddecbcf6a74660fc783bccca9
hyperv <hyperv@ucione.local>
2023-02-07 19:35:46 +0100

    FirsCommit

---
+++ src/UserFun.jsx
@@ -0,0 +1,17 @@
+const users = [
+    {
+        ime: 'Branko',
+        prezime: 'Brankovic',
+        dob: 32,
+    },
+    {
+        ime: 'Janko',
+        prezime: 'Janković',
+        dob: 42,
+    },
+    {
+        ime: 'Stanko',
+        prezime: 'Stanković',
+        dob: 52,
+    }
+]
